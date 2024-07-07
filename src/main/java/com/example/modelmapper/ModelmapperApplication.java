323ffbe09644f7fc7271bc99365b42b6597140a0
spring-boot-react-nextjs <spring-boot-react-nextjs@users.noreply.github.com>
2024-07-07 11:36:45 +0200

    Create Maven Project

---
+++ src/main/java/com/example/modelmapper/ModelmapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.modelmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelmapperApplication.class, args);
+	}
+
+}
